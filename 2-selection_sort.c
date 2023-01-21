6f17dffec48dbe5373fa24052b1ef17fd8f15fbe
uthmanduro <udurosinlohun@gmail.com>
2023-01-21 14:21:24 -0800

    selection

---
+++ 2-selection_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+/**
+ * selection_sort - implementation the selection sort algorithm
+ * @array: the array to sort
+ * @size: size of the aray
+ */
+void selection_sort(int *array, size_t size)
+{
+    size_t temp, index;
+    size_t j, i;
+    for (i = 0; i < size - 1; i++)
+    {
+        temp = i;
+        for (j = i + 1; j < size; j++)
+        {
+
+            if (array[j] < array[temp])
+            {
+                temp = j;
+            }
+        }
+        if (temp != i)
+        {
+            index = array[i];
+            array[i] = array[temp];
+            array[temp] = index;
+            print_array(array, size);
+        }
+    }
+}
