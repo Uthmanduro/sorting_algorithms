21b36775399ab087b564dd676e2745830f35d581
uthmanduro <udurosinlohun@gmail.com>
2023-01-23 17:40:56 -0800

    mandatory task done

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,25 +6,23 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t temp, index;
-    size_t j, i;
-    for (i = 0; i < size - 1; i++)
-    {
-        temp = i;
-        for (j = i + 1; j < size; j++)
-        {
+	size_t temp, index;
+	size_t j, i;
 
-            if (array[j] < array[temp])
-            {
-                temp = j;
-            }
-        }
-        if (temp != i)
-        {
-            index = array[i];
-            array[i] = array[temp];
-            array[temp] = index;
-            print_array(array, size);
-        }
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		temp = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[temp])
+				temp = j;
+		}
+		if (temp != i)
+		{
+			index = array[i];
+			array[i] = array[temp];
+			array[temp] = index;
+			print_array(array, size);
+		}
+	}
 }
